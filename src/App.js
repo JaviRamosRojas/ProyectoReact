56b1a4f2361787ed01a282d862dddafe6b90ba29
JaviRamosRojas <javiera.ramos.rojas@gmail.com>
2021-07-12 19:48:50 -0400

    Cuarto desafio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import NavBar from './components/NavBar.js';
+import NavBar from './components/NavBarComponent/index.js';
+import ItemListContainer from './components/ItemListContainer.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons';
 
 function App() {
   return (
     <div>
      <NavBar />
+     <ItemListContainer greeting={'Haciendo una prop. Acá iría el catálogo.'} />
+
     </div>
   );
 }
